a3c57b0fa916a23882dd6515c6e07094827754a2
Junny Noriega <junnynoriega@gmail.com>
2023-01-18 13:57:54 -0500

    Add home page view

--- index.php
+++ index.php
@@ -17,8 +17,9 @@ $f3 = Base::instance();
 $f3->route('GET /', function () {
 //    echo '<h1>Hello, Fat Free</h1>';
     $view = new Template();
-    echo $view->render(views/home.html);
-});
+    echo $view->render('views/home.html');
+    }
+);
 
 // Run Fat-Free
 $f3->run();
