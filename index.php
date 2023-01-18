739389e5824ed221a8acf9bd464b594aa681ae08
Junny Noriega <junnynoriega@gmail.com>
2023-01-18 13:05:37 -0500

    Pets index

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+
+// This is my CONTROLLER for the hello part
+
+// Turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+// Require the autoload file
+require_once('vendor/autoload.php');
+
+// create an instance of the Base class
+$f3 = Base::instance();
+// Java equivalent -> Base f3 = new Base();
+
+// Define a default route ("home page" for hello project)
+$f3->route('GET /', function () {
+//    echo '<h1>Hello, Fat Free</h1>';
+    $view = new Template();
+    echo $view->render(views/home.html);
+});
+
+// Run Fat-Free
+$f3->run();
+// Java -> f3.run();
